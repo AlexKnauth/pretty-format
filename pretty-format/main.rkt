135038985bf37f96ac5b199ed7523eb8882c3cad
AlexKnauth <alexander@knauth.org>
2017-11-25 23:52:27 -0500

    add pretty-printf, pretty-format, etc.
    the pretty versions of printf, format, etc.

---
+++ pretty-format/main.rkt
@@ -0,0 +1,71 @@
+#lang racket
+
+(provide pretty-fprintf
+         pretty-printf
+         pretty-eprintf
+         pretty-format)
+
+;; This provides the pretty-format that formats values based on a format string,
+;; like `format`.
+
+;; pretty-fprintf is like fprintf
+;; pretty-printf is like printf
+;; pretty-eprintf is like eprintf
+;; pretty-format is like format
+
+(require (rename-in racket/pretty [pretty-format pretty-string]))
+(module+ test
+  (require rackunit))
+
+(struct formatted (fmt vs)
+  #:methods gen:custom-write
+  [(define (write-proc this out mode)
+     (apply fprintf out (formatted-fmt this) (formatted-vs this)))])
+
+(define (pretty-fprintf out fmt . vs)
+  (pretty-print (formatted fmt vs) out))
+
+(define (pretty-printf fmt . vs)
+  (pretty-print (formatted fmt vs)))
+
+(define (pretty-eprintf fmt . vs)
+  (pretty-print (formatted fmt vs) (current-error-port)))
+
+(define (pretty-format fmt . vs)
+  (pretty-string (formatted fmt vs)))
+
+(module+ test
+  (test-case "pretty-format"
+    (check-equal? (parameterize ([pretty-print-columns 70])
+                    (pretty-format
+                     "~v: ~a"
+                     "and the stuff is indented"
+                     '(define (f n)
+                        (for/list ([i (in-range n)] #:when (odd? i))
+                          (* 2 i)))))
+                  #<<"""
+"and the stuff is indented": (define (f n)
+                               (for/list
+                                ((i (in-range n)) #:when (odd? i))
+                                (* 2 i)))
+"""
+                  ))
+
+  (test-case "pretty-fprintf"
+    (define out (open-output-string))
+    (parameterize ([pretty-print-columns 56])
+      (pretty-fprintf out
+                      "~a = ~v"
+                      'x
+                      '(define (f n)
+                         (for/list ([i (in-range n)] #:when (odd? i))
+                           (* 2 i)))))
+    (check-equal? (get-output-string out)
+                  #<<"""
+x = '(define (f n)
+       (for/list
+        ((i (in-range n)) #:when (odd? i))
+        (* 2 i)))
+
+"""
+                  )))
